df15931446d28a1a6f6d5b3043d97372d3adfd85
innitman <viral.thakerar@gmail.com>
2023-07-27 23:41:16 +0100

    working again :)

--- linkchecker.py
+++ linkchecker.py
@@ -2,12 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
 from requests.adapters import HTTPAdapter, Retry
-  
-url = "https://www.amazon.com/nothing_here"
 
 s = requests.Session()
 
-retries = Retry(total=3,
+retries = Retry(total=2,
                 backoff_factor=0.1,
                 status_forcelist=[ 500, 502, 503, 504 ])
 
@@ -20,7 +18,7 @@ def _validate_url(url):
         not_found_links.append(url)
 
 
-with open("index.html") as fp:
+with open("testme.html") as fp:
 	soup = BeautifulSoup(fp, 'html.parser')
 
 	# Create a list containing all links with the root domain.
@@ -32,6 +30,7 @@ for link in links:
     _validate_url(link)
 
 with open("links.txt", "w") as txt_file:
+    txt_file.write("Links with issues \n\n")
     for line in not_found_links:
-        txt_file.write(line + "\n")
+        txt_file.write("From the file testme.html: " + line + "\n")
 
