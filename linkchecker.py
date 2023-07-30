56a2074f19c7014d6174d62f52cd68405bc595a4
innitman <viral.thakerar@gmail.com>
2023-07-30 17:06:10 +0100

    testing

--- linkchecker.py
+++ linkchecker.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
 from requests.adapters import HTTPAdapter, Retry
+import os
 
 s = requests.Session()
 
@@ -10,6 +11,9 @@ retries = Retry(total=2,
                 status_forcelist=[ 500, 502, 503, 504 ])
 
 s.mount('http://', HTTPAdapter(max_retries=retries))
+
+not_found_links = []
+files_in_directory = os.listdir()
   
 def _validate_url(url):
     try:
@@ -17,20 +21,19 @@ def _validate_url(url):
     except:
         not_found_links.append(url)
 
+def check_broken_links_in_given_html(html):
+    with open(html) as fp:
+        soup = BeautifulSoup(fp, 'html.parser')
+    links = [link.get("href") for link in soup.find_all("a")]
+    for link in links:
+        _validate_url(link)
 
-with open("testme.html") as fp:
-	soup = BeautifulSoup(fp, 'html.parser')
+check_broken_links_in_given_html('testme.html')
 
-	# Create a list containing all links with the root domain.
-links = [link.get("href") for link in soup.find_all("a")]
-	# Initialize list for broken links.
-not_found_links = []
+def write_to_file():
+    with open("links.txt", "w") as txt_file:
+        txt_file.write("Links with issues \n\n")
+        for line in not_found_links:
+            txt_file.write(line + "\n")
 
-for link in links:
-    _validate_url(link)
-
-with open("links.txt", "w") as txt_file:
-    txt_file.write("Links with issues \n\n")
-    for line in not_found_links:
-        txt_file.write("From the file testme.html: " + line + "\n")
-
+write_to_file()
---
+++ tests.py
@@ -0,0 +1,28 @@
+import unittest
+import requests
+
+from linkchecker import _validate_url
+
+s = requests.Session()
+
+
+class MyFirstTests(unittest.TestCase):
+
+    def _validate_url(self, url):
+        try:
+            s.get(url, timeout=1)
+        except:
+            return url
+
+    def test_validate_url_with_broken_link(self):
+        not_found_links = []
+        not_found_links.append(self._validate_url("http://www.brokenlinks23423423423asimai.com"))
+        self.assertTrue(not_found_links)
+
+    def test_validate_url_with_valid_link(self):
+        not_found_links = []
+        not_found_links.append(self._validate_url("http://www.brokenlinks23423423423asimai.com"))
+        self.assertTrue(not_found_links)
+
+if __name__ == '__main__':
+    unittest.main()
