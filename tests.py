e6bb85870b1a3900a77f3165c27b76f8083da28c
innitman <viral.thakerar@gmail.com>
2023-08-09 20:23:09 +0100

    adding all html files

--- tests.py
+++ tests.py
@@ -8,15 +8,9 @@ s = requests.Session()
 
 class MyFirstTests(unittest.TestCase):
 
-    def _validate_url(self, url):
-        try:
-            s.get(url, timeout=1)
-        except:
-            return url
-
     def test_validate_url_with_broken_link(self):
         not_found_links = []
-        not_found_links.append(self._validate_url("http://www.brokenlinks23423423423asimai.com"))
+        not_found_links.append(_validate_url("http://www.brokenlinks23423423423asimai.com"))
         self.assertTrue(not_found_links)
 
     def test_validate_url_with_valid_link(self):
